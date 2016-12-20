3e566e1cffc27c6194708a08f77f9875255d8b44
Christian Talmo <talmo.christian@gmail.com>
2016-12-20 11:50:47 +0200

    basic wrollup, miru watching

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import buble from 'rollup-plugin-buble';
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
+  entry: 'js/index.js',
+  dest: 'public/js/main.js',
+  format: 'iife',
   plugins: [
     buble({
       objectAssign: 'Object.assign'
